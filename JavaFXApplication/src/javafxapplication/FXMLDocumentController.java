007c911e9e44de91d76e76f0db71a8e26d5cb47e
Dinodzo <lopatka.antoine@gmail.com>
2014-02-10 19:21:39 +0100

    Test Bouton OK

--- JavaFXApplication/src/javafxapplication/FXMLDocumentController.java
+++ JavaFXApplication/src/javafxapplication/FXMLDocumentController.java
@@ -19,17 +19,29 @@ import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Font;
 
  /**
  *
  * @author Dinodzo
  */
-public class FXMLDocumentController implements Initializable {
-    
 
+
+public class FXMLDocumentController implements Initializable {
+    @FXML
+    private Font x1;
+    
+    @FXML
+    private Button Bouton1;
+    
+    public void actionPerformed(ActionEvent arg0) {
+  if(arg0.getSource() == Bouton1)
+    System.out.println("Vous avez cliqué sur le bouton 1");
+}
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
